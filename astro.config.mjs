dff8ab28eacbb9047150263964c8d3e480cc4e42
Cesar Bernabé Figueroa <CesarBernabe.Figueroa@alu.frt.utn.edu.ar>
2026-09-05 14:44:50 -0300

    fix: configure base path for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://giovannifrontini.com',
+  site: 'https://bernabefigueroa.github.io',
+  base: '/Gf',
   devToolbar: {
     enabled: false,
   },
