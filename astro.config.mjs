9c0bb4938ae11e8ba4f712c7885db1d499b53f13
Cesar Bernabé Figueroa <CesarBernabe.Figueroa@alu.frt.utn.edu.ar>
2026-09-08 17:20:23 -0300

    feat: update site metadata, og tags and title for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://giovanni-psi.vercel.app',
+  site: 'https://giovannifrontini.com',
   devToolbar: {
     enabled: false,
   },
