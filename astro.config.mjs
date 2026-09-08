aeaf8f6968fbce2495611596dd41842651155cee
Cesar Bernabé Figueroa <CesarBernabe.Figueroa@alu.frt.utn.edu.ar>
2026-09-08 16:12:54 -0300

    fix: remove github pages base path for root deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bernabefigueroa.github.io',
-  base: '/Gf',
+  site: 'https://giovanni-psi.vercel.app',
   devToolbar: {
     enabled: false,
   },
